39c579b23fbe89f77769a01f2abc4671c53bb985
Griotold <gotjd9773@naver.com>
2023-08-03 12:27:28 +0900

    UserControllerTest

---
+++ src/test/java/com/griotold/bankshop/web/UserControllerTest.java
@@ -0,0 +1,123 @@
+package com.griotold.bankshop.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.griotold.bankshop.config.dummy.DummyObject;
+import com.griotold.bankshop.dto.user.UserReqDto;
+import com.griotold.bankshop.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.*;
+
+import static com.griotold.bankshop.dto.user.UserReqDto.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class UserControllerTest extends DummyObject {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        dataSetting();
+    }
+
+    private void dataSetting() {
+        userRepository.save(newUser("griotold", "고리오영감"));
+    }
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join_success_test() throws Exception{
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("love");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("love@nate.com");
+        joinReqDto.setFullName("러브");
+        joinReqDto.setAddress("믿음시 소망구 인내동 456번지");
+
+        String requestBody = om.writeValueAsString(joinReqDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/join")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody = " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isCreated());
+
+    }
+    @Test
+    @DisplayName("회원가입 실패 - 이미 있는 username")
+    void join_fail_test() throws Exception {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("griotold");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("griotold@nate.com");
+        joinReqDto.setFullName("고리오영감");
+        joinReqDto.setAddress("행복시 희망구 사랑동 123번지");
+
+        String requestBody = om.writeValueAsString(joinReqDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/join")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+    @Test
+    @DisplayName("정규표현식 체크 - 이메일 형식")
+    void regExp_test() throws Exception{
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("kandela");
+        joinReqDto.setPassword("5678");
+        joinReqDto.setEmail("kandela123");
+        joinReqDto.setFullName("칸델라");
+        joinReqDto.setAddress("세종시 에코구 전등동 456번지");
+
+        String requestBody = om.writeValueAsString(joinReqDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/join")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody = " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+
+    }
+
+}
